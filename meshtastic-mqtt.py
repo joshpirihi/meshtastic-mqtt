54f8d7c36b4215d7e002e9db203ab13a77c341d2
Joshua Pirihi <josh.pirihi@gmail.com>
2021-11-30 20:11:25 +1300

    Update protobufs, also add support for Traccar

--- meshtastic-mqtt.py
+++ meshtastic-mqtt.py
@@ -9,66 +9,74 @@ import mqtt_pb2 as mqtt_pb2
 
 from paho.mqtt import client as mqtt_client
 
+import requests
 
-broker = '10.147.253.250'
-port = 1883
-topic = "msh/1/c/LongSlow/#"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
-# username = 'emqx'
-# password = 'public'
+class MeshtasticMQTT():
 
-
-def connect_mqtt() -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(client_id)
-#    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    broker = '10.147.253.250'
+    port = 1883
+    topic = "msh/1/c/LongSlow/#"
+    # generate client ID with pub prefix randomly
+    client_id = f'python-mqtt-{random.randint(0, 100)}'
+    # username = 'emqx'
+    # password = 'public'
 
 
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
-        #print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        se = mqtt_pb2.ServiceEnvelope()
-        se.ParseFromString(msg.payload)
-        
-        #print(se)
-        mp = se.packet
-        if mp.decoded.portnum == POSITION_APP:
-            pos = mesh_pb2.Position()
-            pos.ParseFromString(mp.decoded.payload)
-            print(getattr(mp, "from"))
-            print(pos)
-            owntracks_payload = {
-                "_type": "location",
-                "lat": pos.latitude_i * 1e-7,
-                "lon": pos.longitude_i * 1e-7,
-                "tst": pos.time,
-                "batt": pos.battery_level,
-                "alt": pos.altitude
-            }
-            if owntracks_payload["lat"] != 0 and owntracks_payload["lon"] != 0:
-                client.publish("owntracks/"+str(getattr(mp, "from"))+"/meshtastic_node", json.dumps(owntracks_payload))
-            #lets also publish the battery directly
-            if pos.battery_level > 0:
-                client.publish("/mesh/"+str(getattr(mp, "from"))+"/battery", pos.battery_level)
+    def connect_mqtt(self) -> mqtt_client:
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
 
-    client.subscribe(topic)
-    client.on_message = on_message
+        client = mqtt_client.Client(self.client_id)
+        client.username_pw_set("user", "pass")
+        client.on_connect = on_connect
+        client.connect(self.broker, self.port)
+        return client
 
 
-def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+    def subscribe(self, client: mqtt_client):
+        def on_message(client, userdata, msg):
+            #print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+            se = mqtt_pb2.ServiceEnvelope()
+            se.ParseFromString(msg.payload)
+            
+            #print(se)
+            mp = se.packet
+            if mp.decoded.portnum == POSITION_APP:
+                pos = mesh_pb2.Position()
+                pos.ParseFromString(mp.decoded.payload)
+                print(getattr(mp, "from"))
+                print(pos)
+                owntracks_payload = {
+                    "_type": "location",
+                    "lat": pos.latitude_i * 1e-7,
+                    "lon": pos.longitude_i * 1e-7,
+                    "tst": pos.time,
+                    "batt": pos.battery_level,
+                    "alt": pos.altitude
+                }
+                if owntracks_payload["lat"] != 0 and owntracks_payload["lon"] != 0:
+                    client.publish("owntracks/"+str(getattr(mp, "from"))+"/meshtastic_node", json.dumps(owntracks_payload))
+                    submitted = requests.get("http://10.147.253.250:5055?id="+str(getattr(mp, "from"))+"&lat="+str(pos.latitude_i * 1e-7)+"&lon="+str(pos.longitude_i * 1e-7)+"&altitude="+str(pos.altitude)+"&battery_level="+str(pos.battery_level))
+                    print(submitted)
+                #lets also publish the battery directly
+                if pos.battery_level > 0:
+                    client.publish("/mesh/"+str(getattr(mp, "from"))+"/battery", pos.battery_level)
 
+        client.subscribe(self.topic)
+        client.on_message = on_message
+
+
+    def run(self):
+        client = self.connect_mqtt()
+        self.subscribe(client)
+        client.loop_forever()
+
+    def initialize(self):
+        self.run(self)
 
 if __name__ == '__main__':
-    run()
+    mm = MeshtasticMQTT()
+    mm.run()
